6992f861099e9bbfaaf54e386b4a0b22a237e342
bmaggiano <brandon.maggiano@gmail.com>
2022-10-11 23:45:29 -0700

    got working to print to index.html in a for loop of data

--- index.js
+++ index.js
@@ -200,19 +200,20 @@ const internQuestion = function internQuestion() {
 
 
 const generateManager = () => {
-    return `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <h1>${arr[0].name}</h1>
-        <h2>${arr[0].id}</h2>
-        <h2>${arr[0].email}</h2>
-        <h2>${arr[0].number}</h2>
-    </body>
-    </html>`
+    let text = ""
+    for (let i = 0; i < arr.length; i++) {
+        text += arr[i].name;
+}
+return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>${text}</h1>
+</body>
+</html>`
 }
